3ce765fa71de4feb67114fd8792c83ab7359aebe
Klemen Hribar <klemen.hribar@live.com>
2024-01-31 20:21:07 +0100

    intital commit
    Initial project configuration

---
+++ src/main/java/com/example/javaexcercises/JavaExcercisesApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaexcercises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaExcercisesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaExcercisesApplication.class, args);
+    }
+
+}
